dc9137333697f72852c1038e6c50231aaa48bfc9
xiayulin123 <xiayulinsky@gmail.com>
2023-08-20 13:05:17 -0400

    edit

---
+++ components/navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import {  Menu } from "lucide-react";
+import Link from 'next/link'
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+    weight: '600',
+    subsets:["latin"]
+})
+export const Navbar = () => {
+    return(
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary ">
+            <div className="felx items-center">
+                <Menu className="block md:hidden" />
+                <Link  href="/">
+                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
+                    font.className
+                    )}>
+                        character.ai
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <UserButton />
+            </div>
+        </div>
+    )
+}

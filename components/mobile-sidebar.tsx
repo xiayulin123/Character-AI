ff2d302a8ae65b37391886b25788c5bf91b679de
xiayulin123 <xiayulinsky@gmail.com>
2023-08-20 23:55:17 -0400

    working

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -6,14 +6,14 @@ import {
     SheetTrigger
 } from '@/components/ui/sheet'
 import { Sidebar } from "@/components/sidebar"
-export const MobileSidebar =()=>{
+export const MobileSidebar =({isPro}:{isPro:boolean})=>{
     return (
         <Sheet>
             <SheetTrigger className="md:hidden pr-4">
                 <Menu />
             </SheetTrigger>
             <SheetContent side='left' className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
+                <Sidebar isPro={isPro}/>
             </SheetContent>
         </Sheet>
     )
